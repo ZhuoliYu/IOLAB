0441c9a2ce0060ccdf7dd8457d09dc823b3ed8c9
Zhuoliyu <FeiFei@LAPTOP-20CQOUBD>
2022-04-02 23:25:59 -0500

    添加项目文件。

---
+++ Directory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOLAB {
+    public class Directory 
+    {
+        public string theMachineStops { get; set; }
+        public string TelegramCopy { get; set; }
+        public Directory()
+        {
+            theMachineStops = @"C:\Users\FeiFei\Desktop\IOLAB\theMachineStops.cs";
+            TelegramCopy = @"C:\Users\FeiFei\Desktop\IOLAB\TelegramCopy.txt";
+        }
+    }
+   
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+
+IOLAB.Directory savedDirectory = new IOLAB.Directory();
+FileInfo theMachineStops = new FileInfo(savedDirectory.theMachineStops);
+
+ReplacePeriodToSTOP(theMachineStops);
+Console.ReadLine();
+
+void ReplacePeriodToSTOP(FileInfo file)
+{
+    try
+    {
+        string result;
+        using (StreamReader sr = file.OpenText())
+        {
+            var Content = sr.ReadToEnd();
+            string[] words = Content.Split('.');
+            result = String.Join("STOP", words);
+        }
+        using (StreamWriter sw = File.CreateText(savedDirectory.TelegramCopy))
+        {
+            sw.WriteLine(result);
+        }
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
